c88b39f7ccf73abd4d245fa446d064ce8d5853f5
Jambi <iturkas42@gmail.com>
2024-03-15 09:23:09 +0200

    Додано показання середнього значення елементів у масиві.

--- Practical/Practical.cpp
+++ Practical/Practical.cpp
@@ -21,18 +21,33 @@ int min = arr[0];
 int max = arr[0];
 
 for (int i = 1; i < size; ++i) {
+
     if (arr[i] < min) {
+
         min = arr[i];
+
     }
     if (arr[i] > max) {
+
         max = arr[i];
+
     }
 }
+
+for (int i = 0; i < size; ++i) {
+
+    sum += arr[i];
+
+}
+
+double aver = static_cast<double>(sum) / size; 
 
 cout << "The sum of the all array elements is " << sum << '\n';
 cout << " " << '\n';
 cout << "The min element of the array is " << min << '\n';
 cout << "The max element of the array is " << max << '\n';
+cout << " " << '\n';
+cout << "The average value of the array elements is " << aver << '\n';
 
 return 0;
 
