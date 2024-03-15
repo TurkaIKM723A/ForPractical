28c58060a2715ad676fe0541d6d86e93b8f9e00e
Jambi <iturkas42@gmail.com>
2024-03-15 09:14:12 +0200

    Додано відображення мінімального та максиального елементу в масиві

--- Practical/Practical.cpp
+++ Practical/Practical.cpp
@@ -17,7 +17,22 @@ for (int i = 0; i < size; ++i) {
 
 }
 
+int min = arr[0];
+int max = arr[0];
+
+for (int i = 1; i < size; ++i) {
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
+
 cout << "The sum of the all array elements is " << sum << '\n';
+cout << " " << '\n';
+cout << "The min element of the array is " << min << '\n';
+cout << "The max element of the array is " << max << '\n';
 
 return 0;
 
